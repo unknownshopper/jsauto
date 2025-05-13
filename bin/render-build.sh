768f2d0db4f2481b4721c80c7650bf9b162c3835
UnknownShopper <the@unknownshoppers.com>
2025-05-13 17:31:40 -0600

    rend10

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 # Install dependencies
 bundle install
 
-# Compile assets
-BUNDLE_WITHOUT=development:test bundle exec rake assets:precompile
-
-# Set up database
+# Setup database
 bundle exec rake db:migrate
-bundle exec rake db:seed
+
+# Precompile assets
+bundle exec rake assets:precompile
