5b4ecd332ec05df1561fa94a98ade1d7d0bca99f
UnknownShopper <the@unknownshoppers.com>
2025-05-13 17:37:37 -0600

    rendre

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Install dependencies
+# Bundle install
+bundle config set --local deployment 'true'
+bundle config set --local without 'development test'
 bundle install
 
-# Setup database
-bundle exec rake db:migrate
+# Database setup
+if [ "$RAILS_ENV" = "production" ]; then
+  bundle exec rake db:migrate
+else
+  bundle exec rake db:setup
+fi
 
-# Precompile assets
-bundle exec rake assets:precompile
+# Asset precompilation
+if [ "$RAILS_ENV" = "production" ]; then
+  bundle exec rake assets:precompile
+fi
